1c029537dc3971a25195a2834aaf85578651b533
jqlliu <2260216963liu@gmail.com>
2024-01-09 22:01:55 -0500

    Added bootstrap lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Home() {
-  
+  return (<Button>Hello</Button>)
 }
