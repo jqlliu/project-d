5dc2a0b0ccc923e99d9a0e4f56396efd1a134c94
jqlliu <2260216963liu@gmail.com>
2024-01-09 22:13:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from 'react';
 
 export default function Home() {
   return (<Button>Hello</Button>)
