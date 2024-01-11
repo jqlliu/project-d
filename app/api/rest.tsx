326d968693a821bb005cd924c1261078b56dbfb8
jqlliu <2260216963liu@gmail.com>
2024-01-10 21:26:22 -0500

    Did some shuffling of things

---
+++ app/api/rest.tsx
@@ -0,0 +1,20 @@
+//This document will have API handler and requester stuff
+//
+
+//Importing
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(request: NextApiRequest, responce: NextApiResponse){
+    responce.status(200).json({text: "HI"})
+    // if(request.method == "POST"){
+    //     responce.send("RECIEVED")
+    // }else if (request.method == "GET"){
+        
+    // }
+}
+
+
+function send(responce: NextApiResponse, body: String){
+    
+    responce.send(body);
+}
