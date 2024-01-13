29ba73326480ca77609ccd5039dab402e7cfc61b
Catherine-JY-Zhang <catherine.jy.zhang@gmail.com>
2024-01-13 16:02:53 -0500

    changed nav to tab

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,26 @@ export default function Home() {
   <div>
       <h1 align="center">Geo Chat</h1>
       <div>
-          <nav className="nav justify-content-center">
-            <a className="nav-link active" aria-current="page" href="#">Inbox</a>
-            <a className="nav-link" href="#">Saved</a>
-            <a className="nav-link" href="#">Drafts</a>
-            <a className="nav-link" href="#">Sent</a>
-            <a className="nav-link" href="#">Spam</a>
-            <a className="nav-link" href="#">Deleted</a>
-          </nav>
+        <ul class="nav nav-tabs justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inbox</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Saved</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Drafts</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sent</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Spam</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Deleted</a>
+          </li>
+        </ul>
           <div>
             <table class="table table-hover">
               <thead class="table-success">
@@ -47,6 +59,114 @@ export default function Home() {
                   <th>hihihihihih</th>
                   <td> </td>
                   <td>yooooo</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+                <tr>
+                  <td scope="row">a</td>
+                  <th>b</th>
+                  <td>c</td>
+                  <td>d</td>
                 </tr>
               </tbody>
             </table>
