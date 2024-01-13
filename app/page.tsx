fec3e3a8085e82c78373e288886f18ab047352d8
Catherine-JY-Zhang <catherine.jy.zhang@gmail.com>
2024-01-13 14:24:03 -0500

    I did a thing :0

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,18 @@ import { useState } from 'react';
 
 export default function Home() {
   return (
-  <div className="text-center">
-  <nav className="nav justify-content-center">
-    <a className="nav-link active" aria-current="page" href="#">Inbox</a>
-    <a className="nav-link" href="#">Saved</a>
-    <a className="nav-link" href="#">Drafts</a>
-    <a className="nav-link" href="#">Sent</a>
-    <a className="nav-link" href="#">Spam</a>
-    <a className="nav-link" href="#">Deleted</a>
-    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-  </nav>
-  </div>)
+  <div>
+      <h1>Geo Messaging</h1>
+      <div>
+          <nav className="nav justify-content-center">
+            <a className="nav-link active" aria-current="page" href="#">Inbox</a>
+            <a className="nav-link" href="#">Saved</a>
+            <a className="nav-link" href="#">Drafts</a>
+            <a className="nav-link" href="#">Sent</a>
+            <a className="nav-link" href="#">Spam</a>
+            <a className="nav-link" href="#">Deleted</a>
+          </nav>
+      </div>
+  </div>
+        )
 }
