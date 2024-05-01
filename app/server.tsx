95f24296aca5f44b7d49351ff5e7ea580f0d32eb
jqlliu <2260216963liu@gmail.com>
2024-04-30 23:35:11 -0400

    Update server.tsx

--- app/server.tsx
+++ app/server.tsx
@@ -2,8 +2,14 @@
 import {Server} from "socket.io";
 import { createServer } from "http";
 const httpServ = createServer();
-const io = new Server(httpServ, {});
+const io = new Server(httpServ, {
+    cors: {
+        origin: "https://localhost:8080"
+    }
+});
 //TODO: Send an event lmao
+
+
 io.on("Connection", (socket)=>{
 
 });
