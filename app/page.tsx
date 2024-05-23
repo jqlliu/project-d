670f8fe867904349b8fd60d131c6922dd40cade6
jqlliu <2260216963liu@gmail.com>
2024-05-23 16:27:51 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           SEND MESSAGE
         </button>
       </div>
-
+{/* 
       <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
@@ -56,7 +56,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div>
         <ul className="nav nav-tabs justify-content-center">
